c4d4b527230d18363397fc2bb335dc5ba5ecd7c2
Luis Angel Ureña Perdomo <urenaperdomo@gmail.com>
2026-04-21 13:17:12 -0400

    Se cambio el EmailSender to Brevo

--- utils/LoadEnvConfig.js
+++ utils/LoadEnvConfig.js
@@ -7,15 +7,6 @@ const envPath = path.join(
   `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ""}`
 );
 
-console.log(`Loading environment variables from ${envPath}`);
-console.log(`Current NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`MONGO_URI: ${process.env.MONGO_URI}`);
-console.log(`DB_HOST: ${process.env.DB_HOST}`);
-console.log(`DB_PORT: ${process.env.DB_PORT}`);
-console.log(`DB_NAME: ${process.env.DB_NAME}`);
-console.log(`Email User: ${process.env.EMAIL_USER}`);
-console.log(`Email Service: ${process.env.EMAIL_SERVICE}`);
-console.log(`Email Pass: ${process.env.EMAIL_PASS}`);
 
 // load environment variables from the .env file
 dotenv.config({ path: envPath });
